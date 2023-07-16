d2a144f040bc610cb9c714889e204801b93bb8b0
Rsiu95 <rsiu95@gmail.com>
2023-07-16 20:16:10 +1000

    Updated lesson 3
    added pre-implementation of optional challenge

--- Jovian Course/lesson3 - Hashing and arrays.py
+++ Jovian Course/lesson3 - Hashing and arrays.py
@@ -204,3 +204,34 @@ probing_table.insert('silent', 200)
 print("colliding",probing_table.find('listen') == 99 and probing_table.find('silent') == 200)
 
 print(probing_table.list_all() == ['listen', 'silent'])
+
+# (Optional) Question: Implement a python-friendly interface for the hash table.
+class HashTable:
+    def __init__(self, max_size=MAX_HASH_TABLE_SIZE):
+        self.data_list = [None] * max_size
+        
+    def get_valid_index(self, key):
+        # Use Python's in-built `hash` function and implement linear probing
+        pass # change this
+        
+    def __getitem__(self, key):
+        # Implement the logic for "find" here
+        pass # change this
+    
+    def __setitem__(self, key, value):
+        # Implement the logic for "insert/update" here
+        pass # change this
+    
+    def __iter__(self):
+        return (x for x in self.data_list if x is not None)
+    
+    def __len__(self):
+        return len([x for x in self])
+    
+    def __repr__(self):
+        from textwrap import indent
+        pairs = [indent("{} : {}".format(repr(kv[0]), repr(kv[1])), '  ') for kv in self]
+        return "{\n" + "{}".format(',\n'.join(pairs)) + "\n}"
+    
+    def __str__(self):
+        return repr(self)
