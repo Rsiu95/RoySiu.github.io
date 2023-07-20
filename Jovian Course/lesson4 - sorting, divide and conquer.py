f112bedf22b50e1d977c71ce21fbcba41aa45dee
Rsiu95 <rsiu95@gmail.com>
2023-07-20 20:26:10 +1000

    Update lesson4 - sorting, divide and conquer.py
    completed lesson 4

--- Jovian Course/lesson4 - sorting, divide and conquer.py
+++ Jovian Course/lesson4 - sorting, divide and conquer.py
@@ -177,3 +177,123 @@ def merge(nums1, nums2):
     
     # Return the final merged array
     return merged + nums1_tail + nums2_tail
+
+# quick sort
+def quicksort(nums, start=0, end=None):
+    # print('quicksort', nums, start, end)
+    if end is None:
+        nums = list(nums)
+        end = len(nums) - 1
+    
+    if start < end:
+        pivot = partition(nums, start, end)
+        quicksort(nums, start, pivot-1)
+        quicksort(nums, pivot+1, end)
+
+    return nums
+
+# partition function
+def partition(nums, start=0, end=None):
+    # print('partition', nums, start, end)
+    if end is None:
+        end = len(nums) - 1
+    
+    # Initialize right and left pointers
+    l, r = start, end-1
+    
+    # Iterate while they're apart
+    while r > l:
+        # print('  ', nums, l, r)
+        # Increment left pointer if number is less or equal to pivot
+        if nums[l] <= nums[end]:
+            l += 1
+        
+        # Decrement right pointer if number is greater than pivot
+        elif nums[r] > nums[end]:
+            r -= 1
+        
+        # Two out-of-place elements found, swap them
+        else:
+            nums[l], nums[r] = nums[r], nums[l]
+    # print('  ', nums, l, r)
+    # Place the pivot between the two parts
+    if nums[l] > nums[end]:
+        nums[l], nums[end] = nums[end], nums[l]
+        return l
+    else:
+        return end
+    
+    '''
+    QUESTION 1: You're working on a new feature on Jovian called 
+    "Top Notebooks of the Week". Write a function to sort a list of 
+    notebooks in decreasing order of likes. Keep in mind that up 
+    to millions of notebooks can be created every week, so your 
+    function needs to be as efficient as possible.
+    '''
+    
+class Notebook:
+    def __init__(self, title, username, likes):
+        self.title, self.username, self.likes = title, username, likes
+            
+    def __repr__(self):
+        return 'Notebook <"{}/{}", {} likes>'.format(self.username, self.title, self.likes)
+    
+nb0 = Notebook('pytorch-basics', 'aakashns', 373)
+nb1 = Notebook('linear-regression', 'siddhant', 532)
+nb2 = Notebook('logistic-regression', 'vikas', 31)
+nb3 = Notebook('feedforward-nn', 'sonaksh', 94)
+nb4 = Notebook('cifar10-cnn', 'biraj', 2)
+nb5 = Notebook('cifar10-resnet', 'tanya', 29)
+nb6 = Notebook('anime-gans', 'hemanth', 80)
+nb7 = Notebook('python-fundamentals', 'vishal', 136)
+nb8 = Notebook('python-functions', 'aakashns', 74)
+nb9 = Notebook('python-numpy', 'siddhant', 92)
+
+notebooks = [nb0, nb1, nb2, nb3, nb4, nb5,nb6, nb7, nb8, nb9]
+
+def compare_likes(nb1, nb2):
+    if nb1.likes > nb2.likes:
+        return 'lesser'
+    elif nb1.likes == nb2.likes:
+        return 'equal'
+    elif nb1.likes < nb2.likes:
+        return 'greater'
+
+    
+def default_compare(x, y):
+    if x < y:
+        return 'less'
+    elif x == y:
+        return 'equal'
+    else:
+        return 'greater'
+
+def merge_sort(objs, compare=default_compare):
+    if len(objs) < 2:
+        return objs
+    mid = len(objs) // 2
+    return merge(merge_sort(objs[:mid], compare), 
+                 merge_sort(objs[mid:], compare), 
+                 compare)
+
+def merge(left, right, compare):
+    i, j, merged = 0, 0, []
+    while i < len(left) and j < len(right):
+        result = compare(left[i], right[j])
+        if result == 'lesser' or result == 'equal':
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    return merged + left[i:] + right[j:]
+
+# comparing/sorting by title
+def compare_titles(nb1, nb2):
+    # sorting in decreasing order
+    if nb1.title < nb2.title:
+        return 'lesser'
+    elif nb1.title == nb2.title:
+        return 'equal'
+    elif nb1.title > nb2.title:
+        return 'greater'
