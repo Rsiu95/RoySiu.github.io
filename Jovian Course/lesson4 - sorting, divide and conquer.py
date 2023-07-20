42afa531246738a061bf2ab5400c86485955eba4
Rsiu95 <rsiu95@gmail.com>
2023-07-20 14:43:31 +1000

    Update lesson4 - sorting, divide and conquer.py
    Looked at merge sort algorithm

--- Jovian Course/lesson4 - sorting, divide and conquer.py
+++ Jovian Course/lesson4 - sorting, divide and conquer.py
@@ -119,7 +119,7 @@ def bubble_sort(nums): # O(n^2) time complexity, O(n) space complexity
 
 # insertion sort
 nums = [23, 2, 1, 4, 11]
-def insertion_sort(nums):
+def insertion_sort(nums): # O(n^2) time, O(1)
     nums = list(nums)
     for i in range(len(nums)):
         cur = nums.pop(i)
@@ -128,3 +128,52 @@ def insertion_sort(nums):
             j -= 1
         nums.insert(j+1, cur)
     return nums        
+
+# Merge Sort > typical divide and conquer algorithm
+# this assumes we have a helper function called "merge"
+
+def merge_sort(nums):
+    # Terminating condition (list of 0 or 1 elements)
+    if len(nums) <= 1:
+        return nums
+    
+    # Get the midpoint
+    mid = len(nums) // 2 # integer division
+    
+    # Split the list into two halves
+    left = nums[:mid]
+    right = nums[mid:]
+    
+    # Solve the problem for each half recursively
+    left_sorted, right_sorted = merge_sort(left), merge_sort(right)
+    
+    # Combine the results of the two halves
+    sorted_nums =  merge(left_sorted, right_sorted)
+    
+    return sorted_nums
+
+# merge helper function
+def merge(nums1, nums2):    
+    # List to store the results 
+    merged = []
+    
+    # Indices for iteration
+    i, j = 0, 0
+    
+    # Loop over the two lists
+    while i < len(nums1) and j < len(nums2):        
+        
+        # Include the smaller element in the result and move to next element
+        if nums1[i] <= nums2[j]:
+            merged.append(nums1[i])
+            i += 1 
+        else:
+            merged.append(nums2[j])
+            j += 1
+    
+    # Get the remaining parts
+    nums1_tail = nums1[i:]
+    nums2_tail = nums2[j:]
+    
+    # Return the final merged array
+    return merged + nums1_tail + nums2_tail
