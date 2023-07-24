6759e1c1ba9d28410f71f6556b6ab1478639d6ed
Rsiu95 <rsiu95@gmail.com>
2023-07-24 10:27:32 +1000

    Create lesson6 - graph algorithms.py
    started learning the graph class
    implemented add_edge and remove_edge myself.

---
+++ Jovian Course/lesson6 - graph algorithms.py
@@ -0,0 +1,85 @@
+'''
+Representing a graph:
+A node is generally the "point" at which you would like to visit
+An edge is the "path" between 2 points.
+
+i.e. general graph data structure
+
+    0 ----- 1 
+    |      /|\
+    |    /  |  \
+    |   /   |   2
+    |  /    |  /
+    | /     | /
+    4 ----- 3
+
+Note that the numbers represent nodes and are just labels, the lines between each are
+the edges
+'''
+# to represent the above structure
+num_nodes = 5
+# edges are bidirectional here, order also doesn't matter
+edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3,4)]
+
+# A better way to create the graph
+# build an adjanceny list
+
+'''
+
+Using the above structure, we track the nodes and store their immediate neighbours
+
+0 --> 1, 4
+1 --> 0, 2, 3, 4
+2 --> 1, 3
+3 --> 1, 2, 4
+4 --> 0, 1, 3
+
+'''
+
+# Create a class to represent a graph as an adjacency list:
+
+class Graph:
+    def __init__(self, num_nodes, edges):
+        self.num_nodes = num_nodes
+        
+        # create the adjacency list, which is empty to be filled
+        self.adjacency_list = [[] for _ in range(num_nodes)]
+        
+        # insert the edge into the appropriate list
+        for node1, node2 in edges:
+            # Add both nodes to each other as it is a bidirectional graph
+            self.adjacency_list[node1].append(node2)
+            self.adjacency_list[node2].append(node1)
+    
+    # create a repr function to display the adjacency list more elegantly        
+    def __repr__(self):
+        return "\n".join(["{}: {}".format(node, neighbours) for \
+            node, neighbours in enumerate(self.adjacency_list)])
+        
+    def __str__(self):
+        return self.__repr__()
+    
+    # write a function to add an edge to a graph represented as an adjacency list
+    def add_edge(self, node1, node2):
+        self.adjacency_list[node1].append(node2)
+        self.adjacency_list[node2].append(node1)
+    
+    # write a function to remove an edge from a graph represented as an adjacency list
+    def remove_edge(self, node1, node2):
+        
+        # check if there is an edge between node 1 and node 2
+        if node2 in self.adjacency_list[node1]:
+            self.adjacency_list[node1].remove(node2)
+        
+        # same as above since it's bidirectional/undirected    
+        if node1 in self.adjacency_list[node2]:
+            self.adjacency_list[node2].remove(node1)
+            
+graph1 = Graph(num_nodes, edges)
+
+print(graph1)
+
+
+
+
+
