4a61950fb700f07f00d72c2fa8d5da4d89a14021
Rsiu95 <rsiu95@gmail.com>
2023-07-25 11:32:28 +1000

    Update lesson6 - graph algorithms.py
    Implemented and looked at Djikstra's algorithm

--- Jovian Course/lesson6 - graph algorithms.py
+++ Jovian Course/lesson6 - graph algorithms.py
@@ -278,3 +278,110 @@ graph2 = Graph(num_nodes5, edges5, weighted = True)
 print(graph2)
 graph3 = Graph(num_nodes6, edges6, directed = True)
 print(graph3)
+
+# shortest path question > Dijkstra's algorithm
+# write a function to find the length of the shortest path between two nodes 
+# in a weighted directed graph. IF NO WEIGHTS USE BFS, IF WEIGHTS USE DIJKSTRA'S
+
+num_nodes4 = 6
+edges4 = [(0, 1, 4), (0, 2, 2), (1, 2, 5), (1, 3, 10), (2, 4, 3), (4, 3, 4), (3, 5, 11)]
+graph4 = Graph(num_nodes4, edges4, weighted = True, directed = True)
+
+
+def shortest_path(graph, start_node, end_node):
+    
+    # set all nodes to unvisited
+    visited = [False] * len(graph.data)
+    
+    # initialise a parent to track the parent of each node
+    parent = [False] * len(graph.data)
+    
+    # set all distances to infinity
+    distance = [float('inf')] * len(graph.data)
+    
+    # create a queue as we are using "first in, first out" structure
+    queue = []
+    
+    # mark distance of source node = 0
+    distance[start_node] = 0
+    
+    # push this starting node into the queue
+    queue.append(start_node)
+    
+    # initialise a pointer to keep track of which elelment to dequeue
+    idx = 0
+    
+    # check if we have gone through every element in the queue and whether or not
+    # we've reached the target node
+    while idx < len(queue) and not visited[end_node]:
+        # Get the current node
+        current = queue[idx]
+
+        # set the visited flag to true
+        visited[current] = True
+        
+        # increment the index so we know to check the next node
+        idx += 1
+        
+        # update the distances of all neighbours of the current node
+        update_distances(graph, current, distance, parent)
+        
+        # find the first unvisited node with the lowest distance
+        next_node = pick_next_node(distance, visited)
+        
+        # add the next node to the queue if there is one
+        if next_node:
+            queue.append(next_node)
+            
+    return distance[end_node], parent
+
+# updates the distances of the current node's neighbours        
+def update_distances(graph, current, distance, parent = None):
+    # check the neighbours of the current node
+    neighbors = graph.data[current]
+    
+    # find the weights of the edges that are connected to the current node
+    weights = graph.weight[current]
+    
+    # check each neighbour
+    for i, node in enumerate(neighbors):
+        
+        # get the weight
+        weight = weights[i]
+        
+        # check the if distance of the current node and the weight are less than
+        # distance of the node we're looking at. This will be infinity if it's unvisited
+        if distance[current] + weight < distance[node]:
+            # set the distance of the node we're looking at to = current distance + edge
+            distance[node] = distance[current] + weight
+            
+            # when we update the distance of a node, we check if that node was a parent
+            # of the node we're looking at. if it is, we update the parent to be the 
+            # current node.
+            if parent:
+                parent[node] = current
+# Pick the next univisited node at the smallest distance             
+def pick_next_node(distance, visited):
+    # track the minimum distanace, so we declare a variable and set it to infinity     
+    min_distance = float('inf')
+    
+    # initialise the node with the minimum distance to None
+    min_node = None
+    
+    # itereate through all the nodes 
+    for node in range(len(distance)):
+        
+        # if we haven't visited this node and the distance of that node is less than
+        # our alread minimum distance, update our node with the minimum distance and
+        # update our minimum distance
+        if not visited[node] and distance[node] < min_distance:
+            min_node = node
+            min_distance = distance[node]
+            
+    # return our node with the lowest distance        
+    return min_node
+print(graph4)
+print(shortest_path(graph4, 0, 5))
+print('\n')
+print(graph2)
+print(shortest_path(graph2, 0, 7))
