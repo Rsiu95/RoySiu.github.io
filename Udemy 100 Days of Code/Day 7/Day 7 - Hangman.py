a08b00ad49be49c1e7f881981c2a93a2f8b7fb95
Rsiu95 <rsiu95@gmail.com>
2023-07-28 10:16:53 +1000

    Completed Hangman Project
    Happy with the solution, likely can improve this by adding a way to store what guesses have already been guessed to make it a little easier and user friendly.

---
+++ Udemy 100 Days of Code/Day 7/Day 7 - Hangman.py
@@ -0,0 +1,81 @@
+from random import randint
+import re
+import hangman_art
+import hangman_words
+
+# Retrieves a random word from hangman_words
+word_list = hangman_words.word_list
+
+# Print the starting hangman ASCII art logo
+print(hangman_art.logo + "\n")
+
+# Set the game state to False and wait for the Enter key 
+# to be pressed before starting the game.
+game_start = False
+input("\nPress Enter to start!\n")
+game_start = True
+
+# Select any random word from the list of words.
+word = word_list[randint(0, len(word_list) - 1)]
+
+# I opted to split the word into a list to store it and check it against the final guessed solution
+split_word = list(" ".join(word))
+split_word.append(" ")
+
+# Show how many letters in the word denoted by _.
+output = "_ " * len(word)
+print(output + "\n")
+
+# Set the lives to 6 as the ASCII hangman has 6 body parts.
+lives = 6
+
+# Main loop
+while lives > 0 and game_start == True:
+      
+    # request user input  
+    guess = input("Guess a letter: ").lower()
+      
+    # find the index of the guess in the word
+    matched_chars = re.finditer(guess, word)
+    char_index = [chars.start() for chars in matched_chars]
+      
+    # store the correct guesses into its own list
+    correct_guesses = list(output)
+      
+    # check that the guess is correct
+    if guess in word and guess not in correct_guesses:
+        
+        # update the stored correct guesses position with our guess
+        for char in char_index:
+            correct_guesses[char * 2] = guess
+            output = "".join(correct_guesses)
+      
+        print(hangman_art.stages[lives] + "\n")
+        print(output + "\n")
+
+        # win check
+        if correct_guesses == split_word:
+            print("You win!")
+            game_start = False
+      
+      # check for letters that had already been guessed
+    elif guess in correct_guesses:
+        print(hangman_art.stages[lives])
+        output = "".join(correct_guesses)
+        print(output + "\n")
+        print(f"You've already guessed the letter {guess}.\n")
+      
+
+    else:
+        # Handles game over
+        if lives == 1:
+            print(hangman_art.stages[0])
+            print(f"You lose! The correct word was {word}")
+            game_start = False
+            
+        # reduce the lives by 1               
+        else:
+            lives -= 1
+            print(hangman_art.stages[lives])
+            print("\n" + output)
+            print(f"\nYou guessed {guess}, that's not in the word. You lose a life.\n")
