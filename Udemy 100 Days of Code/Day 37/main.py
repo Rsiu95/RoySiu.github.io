6aaa9601b5a19c108aea51bf6feaf7f830d33ba0
Rsiu95 <rsiu95@gmail.com>
2023-08-13 18:13:12 +1000

    Completed Day 37
    used post to work with pixela

---
+++ Udemy 100 Days of Code/Day 37/main.py
@@ -0,0 +1,50 @@
+import requests
+import datetime as dt
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+TOKEN = ""
+USERNAME = "roysiu"
+ID = "graph1"
+
+todays_date = dt.datetime.now()
+date = todays_date.strftime("%G%m%d")
+print(date)
+pixela_parameters = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# create user
+# response = requests.post(url = pixela_endpoint, json = pixela_parameters)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": ID,
+    "name": "Coding Graph",
+    "unit": "Commits",
+    "type": "int",
+    "color": "ajisai"
+}
+
+# authentication header instead of parsing through the API token.
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url = graph_endpoint, json = graph_config, headers = headers)
+# print(response.text)
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{ID}"
+
+pixel_config = {
+    "date": date,
+    "quantity": "1"
+}
+
+response = requests.post(url = pixel_endpoint, json = pixel_config, headers = headers)
+print(response.text)
