756a7904cf62ce3e25a77b7876d4deabd293d2a7
Rsiu95 <rsiu95@gmail.com>
2023-08-13 18:12:53 +1000

    Update HelloWorld.py
    created random auto mouse clicker

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,18 @@
-print("Hello World")
+import keyboard
+import pyautogui
+import time
+
+
+def auto_click():
+    count = 0
+    while True:
+        pyautogui.click()
+        count += 1
+        time.sleep(0.5)
+        print(count)
+        if keyboard.is_pressed('esc'):
+            break
+                    
+
+auto_click()
+ 
