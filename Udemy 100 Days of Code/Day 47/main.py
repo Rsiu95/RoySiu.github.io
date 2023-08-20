c02d90260c889963b4a2739b328025392a0d6420
Rsiu95 <rsiu95@gmail.com>
2023-08-20 13:39:02 +1000

    Update main.py
    added where to get the header information for this project

--- Udemy 100 Days of Code/Day 47/main.py
+++ Udemy 100 Days of Code/Day 47/main.py
@@ -11,6 +11,8 @@ MY_EMAIL = os.getenv("MY_EMAIL")
 MY_PASSWORD = os.getenv("MY_PASSWORD")
 
 url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+
+# obtain this from: https://myhttpheader.com/
 header = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
