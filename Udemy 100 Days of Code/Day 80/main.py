c831b64bd5ce62a08663c3b3ced4e866846bb385
Rsiu95 <rsiu95@gmail.com>
2023-09-15 16:49:42 +1000

    Update main.py

--- Udemy 100 Days of Code/Day 80/main.py
+++ Udemy 100 Days of Code/Day 80/main.py
@@ -31,17 +31,61 @@ print("Yearly DF Info\n", df_yearly.describe())
 print("By year\n", df_yearly['deaths']/df_yearly['births'] * 100)
 print(f"Chances of dying in the 1840s in Vienna: {df_yearly['deaths'].sum() / df_yearly['births'].sum() * 100:.3}%")
 
+# convert date to datetime format
 df_monthly.date = pd.to_datetime(df_monthly.date)
 
+# use mdates to apply locators
 years = mdates.YearLocator()
 months = mdates. MonthLocator()
 years_format = mdates.DateFormatter('%Y') 
 
+# plotting
+# plt.figure(figsize=(14,8), dpi=200)
+# plt.title('Total Number of Monthly Births and Deaths', fontsize=18)
+ 
+# ax1 = plt.gca()
+# ax2 = ax1.twinx()
+
+# ax1.set_xlim([df_monthly.date.min(), df_monthly.date.max()])
+# ax1.xaxis.set_major_locator(years)
+# ax1.xaxis.set_major_formatter(years_format)
+# ax1.xaxis.set_minor_locator(months)
+ 
+# ax1.grid(color='grey', linestyle='--')
+ 
+# ax1.plot(df_monthly['date'], 
+#          df_monthly['births'], 
+#          color='skyblue', 
+#          linewidth=3)
+ 
+# ax2.plot(df_monthly['date'], 
+#          df_monthly['deaths'], 
+#          color='crimson', 
+#          linewidth=2, 
+#          linestyle='--')
+ 
+# plt.show()
+
+df_monthly['pct_deaths'] = (df_monthly['deaths'] / df_monthly['births']* 100).round(2)
+
+date = '1846-06-01'
+df_before_1846 = df_monthly.query('date < @date')
+df_after_1846 = df_monthly.query('date >= @date')
+print(df_after_1846)
+print(f"Average death rate before 1846: {(df_before_1846.pct_deaths.sum()/len(df_before_1846)).round(3)} %")
+print(f"Average death rate after 1846: {(df_after_1846.pct_deaths.sum()/len(df_after_1846)).round(3)} %")
+
+# set the date to the index
+monthly_deaths_1846 = df_before_1846.set_index('date')
+
+# create a rolling average of 6
+moving_average = monthly_deaths_1846.rolling(window=6).mean()
+print(moving_average)
+# plotting
 plt.figure(figsize=(14,8), dpi=200)
 plt.title('Total Number of Monthly Births and Deaths', fontsize=18)
  
 ax1 = plt.gca()
-ax2 = ax1.twinx()
 
 ax1.set_xlim([df_monthly.date.min(), df_monthly.date.max()])
 ax1.xaxis.set_major_locator(years)
@@ -50,15 +94,27 @@ ax1.xaxis.set_minor_locator(months)
  
 ax1.grid(color='grey', linestyle='--')
  
-ax1.plot(df_monthly['date'], 
-         df_monthly['births'], 
-         color='skyblue', 
-         linewidth=3)
+ax1.plot(df_before_1846['date'], 
+                df_before_1846['pct_deaths'], 
+                color='skyblue', 
+                linewidth=3,
+                label = 'Before Handwashing')
+
+ax1.plot(moving_average.index, 
+         moving_average['pct_deaths'], 
+         color='black', 
+         linewidth=3,
+         linestyle = '--',
+         label = "Moving Average")
  
-ax2.plot(df_monthly['date'], 
-         df_monthly['deaths'], 
-         color='crimson', 
-         linewidth=2, 
-         linestyle='--')
+ax1.plot(df_after_1846['date'], 
+                df_after_1846['pct_deaths'], 
+                color='crimson', 
+                linewidth=2, 
+                linestyle='--',
+                label = 'After Handwashing')
+
+plt.legend(fontsize=18)
  
 plt.show()
+
