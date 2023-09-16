fc2f5e5d7a6fd37eb0097d6543891df6b18c2a62
Rsiu95 <rsiu95@gmail.com>
2023-09-16 15:40:34 +1000

    Update main.py

--- Udemy 100 Days of Code/Day 81/main.py
+++ Udemy 100 Days of Code/Day 81/main.py
@@ -48,3 +48,83 @@ print(f"The average students per teacher ratio: {ptratio_mean:.2f}")
 # Average Price of Home
 price_mean = data['PRICE'].describe()['mean']
 print(f"The average house price: {price_mean:.2f} Thousand")
+
+# CHAS Feature
+# CHAS - Charles River dummy variable, used to determine if the houses are close to the Charles River as it's possible this influences house prices.
+chas_max = data['CHAS'].describe()['max']
+chas_min = data['CHAS'].describe()['min']
+print(f"The CHAS max is: {chas_max:.2f}\nThe CHAS min is: {chas_min:.2f}")
+
+# Room information
+max_rooms = data['RM'].describe()['max']
+min_rooms = data['RM'].describe()['min']
+print(f"The maximum number of rooms in a dwelling is: {max_rooms:.2f}\nThe minimum number of rooms in a dwelling is: {min_rooms:.2f}")
+
+# Plot Housing Prices
+price_dist = sns.displot(data = data,
+                         x = data.PRICE,
+                         bins = 50,
+                         aspect = 2,
+                         kde = True,
+                         )
+
+plt.title(f'1970s Home Values in Boston. Average: ${(1000*data.PRICE.mean()):.6}')
+plt.xlabel('Price in 000s')
+plt.ylabel('Nr. of Homes')
+plt.show()
+
+# Plot Number of Rooms
+rooms_dist = sns.displot(data = data,
+                         x = data.RM,
+                         bins = 50,
+                         aspect = 2,
+                         kde = True,
+                         )
+
+plt.title(f'1970s Number of Rooms per House in Boston.')
+plt.xlabel('Number of Rooms')
+plt.ylabel('Number of Homes')
+plt.show()
+
+# Plot weighted distances to five Boston employment centres
+dis_dist = sns.displot(data = data,
+                       x = data["DIS"],
+                       bins = 50,
+                       aspect = 2,
+                       kde = True,
+)
+
+plt.title('1970s Housing Weighted Distance to Five Boston Employment Centres')
+plt.xlabel('Weighted Distance')
+plt.ylabel('Number of Homes')
+plt.show()
+
+# Plot index of accessibility to radial highways
+rad_dist = sns.displot(data = data,
+                       x = data["RAD"],
+                       bins = 50,
+                       aspect = 2,
+                       kde = True,
+)
+
+plt.title('1970s Housing Index of Accessibility to Radial Highways')
+plt.xlabel('Index')
+plt.ylabel('Number of Homes')
+plt.show()
+
+# Plot index of accessibility to radial highways
+river_access = data['CHAS'].value_counts()
+
+bar = px.bar(x=['No', 'Yes'],
+             y=river_access.values,
+             color=river_access.values,
+             color_continuous_scale=px.colors.sequential.haline,
+             title='Next to Charles River?')
+
+bar.update_layout(xaxis_title='Property Located Next to the River?', 
+                  yaxis_title='Number of Homes',
+                  coloraxis_showscale=False)
+bar.show()
+
+pair_plot = sns.pairplot(data)
+plt.show()
